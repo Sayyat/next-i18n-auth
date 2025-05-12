b9a2f4a2f29a0faf2bd606a8058089a9e442785b
sayat <sayatraykulov@gmail.com>
2025-05-12 21:00:40 +0500

    misc: remove webpack settings from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,19 +6,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  // eslint: {
-  //     // Warning: This allows production builds to successfully complete even if
-  //     // your project has ESLint errors.
-  //     ignoreDuringBuilds: true,
-  // },
-
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.json$/,
-      type: "json",
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
