f9cd515ac7d956fb444296451eb2d8febaad1416
sayat <sayatraykulov@gmail.com>
2025-05-25 02:03:16 +0500

    refactor: format client.ts and server.ts

--- src/shared/data/env/server.ts
+++ src/shared/data/env/server.ts
@@ -1,11 +1,11 @@
-import {createEnv} from "@t3-oss/env-nextjs";
-import {z} from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
-    emptyStringAsUndefined: true,
-    server: {
-        API_URL: z.string(),
-        AUTH_SECRET: z.string(),
-    },
-    experimental__runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+  server: {
+    API_URL: z.string(),
+    AUTH_SECRET: z.string(),
+  },
+  experimental__runtimeEnv: process.env,
 });
