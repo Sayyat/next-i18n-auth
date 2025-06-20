009ece0eb5333804167460573237d20d713a90f9
sayat <sayatraykulov@gmail.com>
2025-06-20 19:15:08 +0500

    refactor: update server side configuration for i18n

--- src/i18n/lib/server.ts
+++ src/i18n/lib/server.ts
@@ -2,28 +2,35 @@
  * Copyright (c) 2025. Sayat Raykul
  */
 "use server";
-import { createInstance, i18n } from "i18next";
-import { getUserLocale } from "./cookies";
+import { createInstance } from "i18next";
+import { getUserLanguage } from "./cookies";
 import { safeT } from "./safety";
 import { TNamespace, TNamespaceTranslationKeys } from "@/i18n/generated/types";
 import { loadNamespace } from "./loader"; // тот что выше
 import { NAMESPACES } from "@/i18n/generated/namespaces";
+import resourcesToBackend from "i18next-resources-to-backend";
 
 // in server
 async function initI18nextOnce(lng: string, ns: (typeof NAMESPACES)[number]) {
+  const translations = await loadNamespace(lng, ns);
+  console.log({ translations });
   const instance = createInstance(
     {
       lng,
       fallbackLng: "en",
       ns: [ns],
       defaultNS: ns,
-      resources: {},
-      // parseMissingKeyHandler: (key) => key,
-      interpolation: {
-        escapeValue: false,
-        maxReplaces: 1,
-        skipOnVariables: true,
+      resources: {
+        [lng]: {
+          [ns]: translations,
+        },
       },
+      // parseMissingKeyHandler: (key) => key,
+      // interpolation: {
+      //   escapeValue: false,
+      //   maxReplaces: 1,
+      //   skipOnVariables: true,
+      // },
       returnNull: false,
       returnEmptyString: true,
       returnObjects: false,
@@ -32,13 +39,21 @@ async function initI18nextOnce(lng: string, ns: (typeof NAMESPACES)[number]) {
       load: "languageOnly",
     },
     () => {
-      // console.log("after createInstance");
+      console.log("initI18nextOnce Loaded");
     },
   );
 
-  const translations = await loadNamespace(lng, ns);
-  instance.addResourceBundle(lng, ns, translations, true, true);
-  await instance.init();
+  // instance.addResourceBundle(lng, ns, translations, true, true);
+  // await instance.init();
+  // const a = instance.getResourceBundle(lng, ns);
+  // console.log({ a });
+  // instance.use(
+  //   resourcesToBackend((language: string, namespace: string) => {
+  //     console.log({ language, namespace });
+  //     return import(`../locales/${language}/${namespace}.json`);
+  //   }),
+  // );
+
   return instance;
 }
 
@@ -50,7 +65,7 @@ export async function getTranslation<N extends TNamespace>(
     options?: Record<string, unknown>,
   ) => string;
 }> {
-  const language = await getUserLocale();
+  const language = await getUserLanguage();
   const i18nextInstance = await initI18nextOnce(language, ns);
   const rawT = i18nextInstance.getFixedT(language, ns);
   const t = safeT(rawT);
