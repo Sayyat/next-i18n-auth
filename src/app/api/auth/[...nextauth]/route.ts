881e7cbd7ca35e3357e4af856fb703dccd03a33a
sayat <sayatraykulov@gmail.com>
2025-06-26 15:57:59 +0500

    chore: format the files

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
-import { handlers } from "@/auth" // Referring to the auth.ts we just created
-export const { GET, POST } = handlers
+/*
+ * Copyright (c) 2025. Sayat Raykul
+ */
+
+import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+export const { GET, POST } = handlers;
