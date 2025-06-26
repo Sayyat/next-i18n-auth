1deee713fc3b4ba9d14beb1a02d80bc402f1e96e
sayat <sayatraykulov@gmail.com>
2025-06-26 15:56:34 +0500

    refactor: update shared/services usages

--- src/features/authentication/services/server.ts
+++ src/features/authentication/services/server.ts
@@ -1,20 +1,23 @@
+/*
+ * Copyright (c) 2025. Sayat Raykul
+ */
+
+"use server";
 import { IResponse, IUser } from "@/shared";
-import { getCentralApi } from "@/shared/services/server";
+import { postWithHandle } from "@/shared/services/server";
 import {
   ILoginPayload,
   IRegisterPayload,
 } from "@/features/authentication/types/payload";
 import { IToken } from "@/features/authentication/types/response";
 
-const api = getCentralApi();
-
 export const registerUser = (
   payload: IRegisterPayload,
 ): Promise<IResponse<IUser>> =>
-  api.postWithHandle<IUser>("/api/auth/register/", payload);
+  postWithHandle<IUser>("/api/auth/register/", payload);
 
 export const loginUser = (payload: ILoginPayload): Promise<IResponse<IUser>> =>
-  api.postWithHandle<IUser>("/api/auth/login/", payload);
+  postWithHandle<IUser>("/api/auth/login/", payload);
 
 export const refreshToken = (refresh: string): Promise<IResponse<IToken>> =>
-  api.postWithHandle<IToken>("/api/auth/token/refresh/", { refresh });
+  postWithHandle<IToken>("/api/auth/token/refresh/", { refresh });
